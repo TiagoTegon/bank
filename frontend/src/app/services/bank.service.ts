7f19b7ecb836b03cdb0743e1bf5ce6206dbb5961
Tiago Júnio Tegon Nascimento <tiago.tegon@gmail.com>
2023-01-26 17:07:11 -0300

    feat: bank service and model

---
+++ frontend/src/app/services/bank.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Bank } from '../shared/models/Bank';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BankService {
+
+  apiUrl = 'https://brasilapi.com.br/api/banks/v1';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getAll():Observable<Bank[]> {
+    return this.httpClient.get<Bank[]>(this.apiUrl)
+  }
+}
---
+++ frontend/src/app/shared/models/Bank.ts
@@ -0,0 +1,6 @@
+export class Bank {
+  code!: number;
+  nome!: string;
+  ispb!: string;
+  fullName!: string;
+}
